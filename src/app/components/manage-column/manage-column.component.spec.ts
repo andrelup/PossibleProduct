31c73f4a6bdf9c1db5548b2a6e3fca30ac30fe2b
André Pereira <andrelcop@gmail.com>
2022-10-30 13:40:33 +0100

    tests: added unit test for ProductService

--- src/app/components/manage-column/manage-column.component.spec.ts
+++ src/app/components/manage-column/manage-column.component.spec.ts
@@ -1,7 +1,5 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material/dialog';
-import { ProductService } from 'src/app/services/product.service';
+
 import { AppModule } from '../../app.module';
 import { ManageColumnComponent } from './manage-column.component';
 
@@ -12,8 +10,7 @@ describe('ManageColumnComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ManageColumnComponent],
-      providers: [ProductService],
-      imports: [MatDialogModule, DragDropModule]
+      imports: [AppModule]
     })
       .compileComponents();
 
--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Product } from '../interfaces/product';
 
 import { ProductService } from './product.service';
 
@@ -12,5 +13,32 @@ describe('ProductService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('obtaining productList', () => {
+    const dataList = service.getProducts();
+    expect(dataList.length).toBeGreaterThan(0);
+    expect(dataList.length).not.toBe(0);
+  });
+  it('add one product', () => {
+    let lengthBeforeAdd = service.getProducts().length;
+    const productToAdd: Product = {
+      name: 'name',
+      price: 1.5,
+      format: 'format',
+      brand: 'brand'
+    }
+    service.addProduct(productToAdd);
+    let products = service.getProducts();
+    expect(products.length).toBe(lengthBeforeAdd + 1);
+    expect(products.includes(productToAdd)).toBeTrue()
+  });
+  it('remove one product', () => {
+    let productsBefore = service.getProducts();
+    let lengthBeforeAdd = productsBefore.length;
+    let indexToDelete = 1;
+    service.removeProduct(indexToDelete);
+    let productsAfter = service.getProducts();
+    expect(productsAfter.length).toBe(lengthBeforeAdd - 1);
+    expect(productsAfter.includes(productsBefore[indexToDelete])).toBeFalse();
   });
 });
