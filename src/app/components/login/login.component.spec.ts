ffec74c5cd2be94c5e49528008a7987573c19fc8
André Pereira <andrelcop@gmail.com>
2022-10-30 14:30:56 +0100

    tests: added unit test for LoginComponent

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,24 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StorageService } from 'src/app/services/storage.service';
 import { AppModule } from '../../app.module';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
+
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
       imports: [AppModule]
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('return invalid form', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    const user = component.loginForm.controls['username'];
+    user.setValue('admin23');
+    expect(component.loginForm.invalid).toBe(true);
+  });
+
+  it('return valid form', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    let user = app.loginForm.controls['username'];
+    let password = app.loginForm.controls['password'];
+
+    user.setValue('admin');
+    password.setValue('admin');
+    expect(app.loginForm.invalid).toBeFalse();
+  });
+
 });
